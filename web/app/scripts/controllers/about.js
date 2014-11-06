cd48adb1b42a847db422cd94b29d183024297b18
ykyoon <xyzlast@gmail.com>
2014-11-06 13:38:24 +0900

    add new project in github

---
+++ web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sadariInfoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sadariInfoApp
+ */
+angular.module('sadariInfoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sadariInfoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sadariInfoApp
+ */
+angular.module('sadariInfoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
